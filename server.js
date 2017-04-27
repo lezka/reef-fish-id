f71dd3544dfed59021197b44ff15351dddba5d3e
David BF <burretfresket@gmail.com>
2017-04-27 20:24:01 +0200

    Dockerize project

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express'),
   app = express(),
-  port = process.env.PORT || 3000,
+  default_port = 3000,
+  port = process.env.PORT || default_port,
+  default_database = 'mongodb://localhost/Fishdb',
+  database = process.env.DATABASE || default_database,
   mongoose = require('mongoose'),
   Task = require('./api/models/fishModel'),
   bodyParser = require('body-parser');
-  
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/Fishdb'); 
+mongoose.connect(database);
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
